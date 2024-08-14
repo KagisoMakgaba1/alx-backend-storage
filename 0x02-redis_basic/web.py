5112cdf408b4a3cf128f0d93b6e19fde3125b903
kagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-08-14 20:51:12 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,48 +3,34 @@
 web cache and tracker
 """
 import requests
-import time
+import redis
 from functools import wraps
 
-
-# Dictionary to cache responses and their timestamps
-cache = {}
-# Dictionary to track access counts
-access_counts = {}
-# Cache expiration time in seconds
-CACHE_EXPIRATION = 10
+store = redis.Redis()
 
 
-def cache_decorator(func):
-    @wraps(func)
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
     def wrapper(url):
-        current_time = time.time()
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
-        # Check if the URL is already cached and if it is still valid
-        if url in cache:
-            cache_entry, timestamp = cache[url]
-            if current_time - timestamp < CACHE_EXPIRATION:
-                print(f"Cache hit for URL: {url}")
-                return cache_entry
+        count_key = "count:" + url
+        html = method(url)
 
-        # If not cached or expired, call the function to fetch the page
-        print(f"Cache miss for URL: {url}")
-        response = func(url)
-        cache[url] = (response, current_time)
-        return response
-
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@cache_decorator
+@count_url_access
 def get_page(url: str) -> str:
-    # Track the number of times the URL has been accessed
-    if url in access_counts:
-        access_counts[url] += 1
-    else:
-        access_counts[url] = 1
-
-    # Fetch the content using requests
-    response = requests.get(url)
-    response.raise_for_status()  # Ensure we raise an error for bad responses
-    return response.text
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
